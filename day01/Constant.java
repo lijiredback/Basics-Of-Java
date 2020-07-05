b1c339435a7f242ccbfaaa04c3d23b5fd53b217c
liji <lijiredback@163.com>
2020-07-05 22:47:11 +0800

    feat: day01 constant

---
+++ day01/Constant.java
@@ -0,0 +1,10 @@
+public class Constant {
+    public static void main(String[] args) {
+        System.out.println("Hello"); // 字符串常量
+        System.out.println(100); // 整数常量
+        System.out.println(10.1); // 小数常量
+        System.out.println(true); // 布尔常量
+        System.out.println('A'); // 字符常量
+        // 空常量：null   无法打印
+    }
+}
