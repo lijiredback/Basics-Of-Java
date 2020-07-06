67fe2ca77c17370a40c0e1a616985d3bcfce9532
lijiredback <lijiredback@lijiredbacks-iMac.local>
2020-07-06 23:38:38 +0800

    feat: day01 Variable

---
+++ day01/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+    public static void main(String[] args) {
+        // 定义变量: 数据类型 变量名 = 数据值;
+
+        // 整数类型变量
+        int i = 20;
+        System.out.println(i);
+
+        // 浮点数类型变量
+        double d = 11.2;
+        System.out.println(d);
+
+        // 字符类型变量
+        char c = 'a';
+        System.out.println(c);
+
+        // 布尔类型变量
+        boolean b = true;
+        System.out.println(b);
+    }
+}
